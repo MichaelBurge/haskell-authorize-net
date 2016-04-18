2a7547bdab9f628d6191fca8ab8437ee83886a38
Michael Burge <michaelburge@pobox.com>
2016-04-17 23:55:02 -0700

    Strip the BOM that Authorize.NET sometimes returns

--- src/Network/AuthorizeNet/Api.hs
+++ src/Network/AuthorizeNet/Api.hs
@@ -4,8 +4,11 @@ module Network.AuthorizeNet.Api where
 
 import Control.Lens
 import Data.Aeson
+import Data.Char
 
 import qualified Data.ByteString.Lazy as BSL
+import qualified Data.Text.Lazy as TL
+import qualified Data.Text.Lazy.Encoding as TL
 
 import Network.AuthorizeNet.Request
 import Network.AuthorizeNet.Response
@@ -27,10 +30,22 @@ productionApiConfig = ApiConfig {
   apiConfig_hostedProfileUrl = "https://secure.authorize.net/profile/manage"
   }
 
-makeRequest :: ApiConfig -> ApiRequest -> IO (Either String ApiResponse)
+stripBom :: BSL.ByteString -> BSL.ByteString
+stripBom bsl = case BSL.splitAt 1 bsl of
+  (bom, xs) | bom == BSL.pack [ 0xFF ] -> xs
+--(bom, xs) | bom == BSL.pack [ 0xEF, 0xBB, 0xBF] -> xs
+  _ -> bsl
+
+-- | Makes an Authorize.NET request, hopefully returning an ApiResponse. If an error occurs, returns the raw response body and the Aeson parse error.
+makeRequest :: ApiConfig -> ApiRequest -> IO (Either (BSL.ByteString, String) ApiResponse)
 makeRequest apiConfig request = do
   response <- post (apiConfig_baseUrl apiConfig) $ toJSON request
   let mBsl = response ^? responseBody
   case mBsl of
-    Nothing -> return $ Left "No response in body"
-    Just bsl -> return $ decodeRequestResponse request bsl
+    Nothing -> return $ Left (BSL.empty, "No response in body")
+    Just bsl ->
+      let strippedBsl = stripBom bsl
+      in case decodeRequestResponse request strippedBsl of
+        Left e -> return $ Left (strippedBsl, e)
+        Right x -> return $ Right x
+  
--- test/Test/Network/AuthorizeNet/Api.hs
+++ test/Test/Network/AuthorizeNet/Api.hs
@@ -2,13 +2,25 @@
 
 module Test.Network.AuthorizeNet.Api (authorizeNetTests) where
 
+import qualified Data.ByteString.Lazy
+
+import Network.AuthorizeNet.Api
+
 import Test.Network.AuthorizeNet.Request
 import Test.Network.AuthorizeNet.Response
 
 import Test.Tasty
+import Test.Tasty.HUnit
+
+test_stripBom :: Assertion
+test_stripBom =
+  let expected = "{\"messages\":{\"resultCode\":\"Error\",\"message\":[{\"code\":\"E00003\",\"text\":\"The element 'getHostedProfilePageRequest' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd' has invalid child element 'customerProfileId' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd'. List of possible elements expected: 'merchantAuthentication' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd'.\"}]}}"
+      actual = "\65279{\"messages\":{\"resultCode\":\"Error\",\"message\":[{\"code\":\"E00003\",\"text\":\"The element 'getHostedProfilePageRequest' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd' has invalid child element 'customerProfileId' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd'. List of possible elements expected: 'merchantAuthentication' in namespace 'AnetApi/xml/v1/schema/AnetApiSchema.xsd'.\"}]}}"
+  in assertEqual "BOM is stripped" expected $ stripBom actual
 
 authorizeNetTests :: TestTree
 authorizeNetTests = testGroup "Authorize.NET tests" [
   requestTests,
-  responseTests
+  responseTests,
+  testCase "stripBom" test_stripBom
   ]
